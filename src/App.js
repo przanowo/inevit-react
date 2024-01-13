dec3c0f2c884f5f88213a707ec915aa6bebd4ece
przanowo <oskar.przanowski@gmail.com>
2024-01-13 23:36:33 +0100

    about

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
 function App() {
   const parallaxRef = useRef(null)
   let animationFrameId = useRef(null)
+  const [activeContent, setActiveContent] = useState('home')
 
   useEffect(() => {
     const parallaxElements = parallaxRef.current.querySelectorAll('.parallax')
@@ -33,6 +34,10 @@ function App() {
       cancelAnimationFrame(animationFrameId.current)
     }
   }, [])
+
+  const handleNavClick = (content) => {
+    setActiveContent(content)
+  }
   return (
     <div className='App' ref={parallaxRef}>
       <header>
@@ -41,70 +46,145 @@ function App() {
             <img src='img/inevit.png' alt='inevit' className='logo' />
           </a>
           <ul>
-            <li className='about'>
-              <a href='pages/about'>About</a>
+            <li className='about' onClick={() => handleNavClick('about')}>
+              about
             </li>
-            <li className='webdev'>
-              <a href='pages/web-dev'>Software Development</a>
+            <li className='webdev' onClick={() => handleNavClick('webdev')}>
+              Software Development
             </li>
-            <li className='translate'>
-              <a href='pages/web-dev'>PL-HU Translating</a>
+            <li
+              className='translate'
+              onClick={() => handleNavClick('translate')}
+            >
+              PL-HU Translating
             </li>
-            <li className='wholesale'>
-              <a href='pages/web-dev'>B2B Wholesale</a>
+            <li
+              className='wholesale'
+              onClick={() => handleNavClick('wholesale')}
+            >
+              B2B Wholesale
             </li>
           </ul>
         </nav>
       </header>
 
       <main>
-        <img
-          src='img/bg.png'
-          alt='background'
-          className='parallax bg'
-          data-speedx='0.15'
-          data-speedy='0.15'
-          data-distane='1001'
-        />
-        <img
-          src='img/middle.png'
-          alt='middle'
-          className='parallax middle'
-          data-speedx='0.08'
-          data-speedy='0.08'
-          data-distane='1001'
-        />
-        <img
-          src='img/rightup.png'
-          alt='rightup'
-          className='parallax rightup-img'
-          data-speedx='0.08'
-          data-speedy='0.08'
-        />
-        <div className='text parallax' data-speedx='0.05' data-speedy='0.05'>
-          <h1>inevit</h1>
-        </div>
-        <img
-          src='img/left.png'
-          alt='left'
-          className='parallax left-img'
-          data-speedx='0.03'
-          data-speedy='0.03'
-          data-distane='1001'
-        />
-        <img
-          src='img/right.png'
-          alt='right'
-          className='parallax right-img'
-          data-speedx='0.03'
-          data-speedy='0.03'
-          data-distane='1001'
-        />
-        <img
-          src='img/blurbottom.png'
-          alt='blurbottom'
-          className='blurbottom-img'
-        />
+        {activeContent === 'home' && (
+          <>
+            <img
+              src='img/bg.png'
+              alt='background'
+              className='parallax bg'
+              data-speedx='0.15'
+              data-speedy='0.15'
+            />
+            <img
+              src='img/middle.png'
+              alt='middle'
+              className='parallax middle'
+              data-speedx='0.08'
+              data-speedy='0.08'
+            />
+            <img
+              src='img/rightup.png'
+              alt='rightup'
+              className='parallax rightup-img'
+              data-speedx='0.08'
+              data-speedy='0.08'
+            />
+            <div
+              className='text parallax'
+              data-speedx='0.05'
+              data-speedy='0.05'
+            >
+              <h1>inevit</h1>
+            </div>
+            <img
+              src='img/left.png'
+              alt='left'
+              className='parallax left-img'
+              data-speedx='0.03'
+              data-speedy='0.03'
+            />
+            <img
+              src='img/right.png'
+              alt='right'
+              className='parallax right-img'
+              data-speedx='0.03'
+              data-speedy='0.03'
+            />
+            <img
+              src='img/blurbottom.png'
+              alt='blurbottom'
+              className='blurbottom-img'
+            />
+          </>
+        )}
+        {activeContent === 'about' && (
+          <>
+            <img
+              src='img/bg.png'
+              alt='background'
+              className='parallax bg'
+              data-speedx='0.15'
+              data-speedy='0.15'
+            />
+            <img
+              src='img/middle.png'
+              alt='middle'
+              className='parallax middle-1'
+              data-speedx='0.08'
+              data-speedy='0.08'
+            />
+            <p className='about-text'>
+              Born in Poland and raised in Hungary, my journey is a blend of
+              these two rich heritages. My early years were shaped by assisting
+              my parents in their Hungarian shops, importing goods from Poland.{' '}
+              <br></br>This experience not only made me fluent in Polish and
+              Hungarian but also ignited my passion for commercial translation
+              and business. <br></br>I specialize in non-document commercial
+              translations between Polish and Hungarian, understanding the
+              nuances of business communication. <br></br>Alongside my
+              linguistic skills, I've developed expertise in web development
+              with a focus on MERN stack technology, crafting e-commerce
+              webshops and engaging landing pages. <br></br>My entrepreneurial
+              spirit led me to establish a wholesale company facilitating
+              tailored import-export services between Poland and Hungary. This
+              venture combines my cultural understanding, language proficiency,
+              and business acumen to support seamless cross-border trade.
+              <br></br>
+              Welcome to my world, where language meets technology and business,
+              creating a hub of opportunities for success and connection.
+            </p>
+            <img
+              src='img/rightup.png'
+              alt='rightup'
+              className='parallax rightup-img-1'
+              data-speedx='0.08'
+              data-speedy='0.08'
+            />
+
+            <img
+              src='img/left.png'
+              alt='left'
+              className='parallax left-img-1'
+              data-speedx='0.03'
+              data-speedy='0.03'
+            />
+            <img
+              src='img/right.png'
+              alt='right'
+              className='parallax right-img-1'
+              data-speedx='0.03'
+              data-speedy='0.03'
+            />
+            <img
+              src='img/blurbottom.png'
+              alt='blurbottom'
+              className='blurbottom-img'
+            />
+          </>
+        )}
       </main>
     </div>
   )
